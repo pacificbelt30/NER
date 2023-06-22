0950bdbd4bdf42ec6db1e74d23704d61e0c69a76
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2023-06-22 15:25:32 +0900

    [add] launcher

---
+++ launcher.py
@@ -0,0 +1,53 @@
+import sys
+import os
+from ner import CombinePDF, TextraConnection
+
+def help():
+    helptxt = """
+    this is launcher for using some NER features.
+    if you want to know feature details:
+        mner.py [subcommand] --help
+
+    after translate( or combine ) subcommand toward [filename].[ext], we find [filename]_new.[ext]( or [filename]_all.[ext] ).
+    subcommands:
+        translate
+        combine
+    """
+    print(helptxt)
+
+def translate():
+    filename = 'source.pdf'
+    if len(sys.argv) >= 3:
+        filename = sys.argv[2]
+    tc = TextraConnection(filename)
+    tc.get_pdf_info()
+    tc.post_page()
+    tc.insert()
+
+def combine():
+    pdf = 'source.pdf'
+    if len(sys.argv) >= 3:
+        pdf = sys.argv[2]
+    file_ext = os.path.basename(pdf).split('.')
+    dir = os.path.dirname(pdf)
+    trans_file = os.path.join(dir,f"{file_ext[0]}_new.{file_ext[1]}")
+    target_file = os.path.join(dir,f"{file_ext[0]}_all.{file_ext[1]}")
+    cpdf = CombinePDF(pdf,trans_file,target_file)
+    cpdf.combine(insert_first_page=False)
+
+def main():
+    if len(sys.argv) <= 1:
+        help()
+        exit(0)
+
+    match sys.argv[1]:
+        case 'translate':
+            translate()
+        case 'combine':
+            combine()
+        case _:
+            help()
+            exit(0)
+
+if __name__ == "__main__":
+    main()
